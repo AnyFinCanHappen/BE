dc173a047c28e3a3161da8ab4e889c32a1e444d1
Sidney Tan <stan@theindoorlab.com>
2023-09-13 16:14:46 -0700

    Initial commit

---
+++ models/Coordinate.cs
@@ -0,0 +1,10 @@
+class Coordinate {
+
+  public Coordinate(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  double x { get; set; }
+  double y { get; set; }
+}
---
+++ models/Region.cs
@@ -0,0 +1,9 @@
+class Region {
+
+  public Region(Coordinate[] points) 
+  {
+    this.points = points;
+  }
+
+  Coordinate[] points { get; set; }
+}
