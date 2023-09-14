30d792f156c97961aa4ed5e822ae650719b9998d
Sidney Tan <stan@theindoorlab.com>
2023-09-14 08:43:18 -0700

    Adjust property name

--- models/Coordinate.cs
+++ models/Coordinate.cs
@@ -1,10 +1,10 @@
 class Coordinate {
 
   public Coordinate(double x, double y) {
-    this.x = x;
+    this.X = x;
     this.y = y;
   }
 
-  double x { get; set; }
-  double y { get; set; }
+  double X { get; set; }
+  double Y { get; set; }
 }
